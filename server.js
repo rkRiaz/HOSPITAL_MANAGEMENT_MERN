b32f1226f062803aabb1761d78cbdc5c452f2515
rkRiaz <mdriaz9586@gmail.com>
2020-12-19 00:07:34 +0600

    node update

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const app = express()
 
 const PORT = process.env.PORT || 8080
-const MONGO_DB_URI = `mongodb://localhost:27017/hospital_management?readPreference=primary&appname=MongoDB%20Compass&ssl=false`
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/hospital_management?readPreference=primary&appname=MongoDB%20Compass&ssl=false`
 
-mongoose.connect(MONGO_DB_URI,
+mongoose.connect(MONGODB_URI,
         {useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
